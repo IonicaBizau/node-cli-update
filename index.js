a7b4635263ad222f91b5770387621d4d8dc61dfd
Ionică Bizău <bizauionica@yahoo.com>
2014-08-12 14:27:19 +0300

    Added back and next methods

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-var CLIUpdate = {};
-CLIUpdate.render = function (output) {
-    // TODO Check history
+var CLIUpdate = module.exports = {};
+CLIUpdate.current = 0;
+CLIUpdate.history = [];
+CLIUpdate.render = function (output, pushHistory) {
+    if (pushHistory !== false) {
+        CLIUpdate.history.push(output);
+        CLIUpdate.current = CLIUpdate.history.length - 1;
+    }
     console.log(output);
+};
+
+CLIUpdate.back = function () {
+    var o = CLIUpdate.history[--CLIUpdate.current];
+    //console.log("------------", CLIUpdate.current, CLIUpdate.history.length);
+    if (!o) { return ++CLIUpdate.current; }
+    CLIUpdate.render(o, true);
+};
+
+CLIUpdate.next = function () {
+    var o = CLIUpdate.history[++CLIUpdate.current];
+    if (!o) { return --CLIUpdate.current; }
+    CLIUpdate.render(o, true);
 };
