b0fe5bdc3bb6c7fda016d0682b7eaa2c36bfda6b
Ionică Bizău <bizauionica@yahoo.com>
2014-12-09 18:37:32 +0200

    Show time

--- test/index.js
+++ test/index.js
@@ -1,8 +1,41 @@
 var CliUpdate = require("../index")
   , CliBox = require("cli-box")
   , c = require("couleurs")(true)
+  , Figlet = require("figlet")
   ;
 
-for (var i = 0; i <= 0; ++i) {
-    CliUpdate.render((new CliBox("30x15", "Nr. ".fg(255, 0, 0) + i).toString()));
+// http://stackoverflow.com/a/16426519/1420197
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return hour + " : " + min + " : " + sec;
 }
+
+setInterval(function () {
+    Figlet(getDateTime(), function(err, data) {
+        data = data.split("\n").map(function (c) { return c.bg("#c0392b") + "\u001b[45m"; }).join("\n");
+        CliUpdate.render(
+            new CliBox(process.stdout.columns + "x" + (process.stdout.rows - 3), data).toString().split("\n").map(function (c) {
+                return c.bg("#2980b9");
+            }).join("\n")
+        );
+    });
+}, 1000);
